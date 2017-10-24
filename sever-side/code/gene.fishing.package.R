20f906e93faeea2a8b563fe2b45ed2b366de6bb3
Ke Liu <keliu.iluke@gmail.com>
2017-10-24 12:35:45 -0700

    V2

--- sever-side/code/gene.fishing.package.R
+++ sever-side/code/gene.fishing.package.R
@@ -6,7 +6,7 @@ require(LICORS)
 
 
 
-get.spectral.clustering.coordinates <- function(A,force.no.of.cluster=0){ 
+get.spectral.clustering.coordinates <- function(A,no.of.eigen.vector=0){ 
     diag(A)  <-   0
     A        <-  abs(A)
     d        <-  apply(A,1,sum)
@@ -19,22 +19,23 @@ get.spectral.clustering.coordinates <- function(A,force.no.of.cluster=0){
     eigen.values    <-  tmp$values[length(tmp$values):1]
     eigen.vectors   <-  tmp$vectors[,length(tmp$values):1]
     
+    #automaticaly choose number of eigen-vectors
     q1              <-  quantile(eigen.values)[2]
     q3              <-  quantile(eigen.values)[4]
     low             <-  q1 - 3*(q3 - q1)
     up              <-  q1 + 3*(q3 - q1)
     k               <-  sum(eigen.values < low) # pick out eigen-values that are relatively small
     
-    if(force.no.of.cluster > 1){
-        k <- force.no.of.cluster
+    if(no.of.eigen.vector > 1){
+        k <- no.of.eigen.vector
     }
     if(k == 1){
-        list(eigen.values=eigen.values,no.of.cluster=1)
+        list(eigen.values=eigen.values,no.of.eigen.vector=1)
     }else{
-       coordinate.matrix            <- eigen.vectors[,2:(k+1)] # pick out the eigen-vectors associated with the K smallest non-zero eigen-values
+       coordinate.matrix            <- eigen.vectors[,2:(k+1)] # pick out the eigen-vectors associated with the k smallest non-zero eigen-values
        rownames(coordinate.matrix)  <- rownames(A)
-       colnames(coordinate.matrix)  <- paste('dim',1:(k),sep="-")
-       list(coordinates=coordinate.matrix,eigen.values=eigen.values,no.of.cluster=k)
+       colnames(coordinate.matrix)  <- paste('eigen',1:(k),sep="-")
+       list(coordinates=coordinate.matrix,eigen.values=eigen.values,no.of.eigen.vector=k)
     }
 }
 
@@ -44,31 +45,32 @@ probe.fishability <- function(bait.gene, expr.matrix,alpha=5){
     bait.gene <- bait.gene[bait.gene %in% colnames(expr.matrix)]
     all.gene  <- colnames(expr.matrix)      
     pool.gene <- setdiff(all.gene,bait.gene)
-    bait.cluster.purity.matrix <- foreach(k =c(2:10),.combine='cbind') %do%{
-        bait.cluster.purity    <- foreach(i= 1:1000,.combine='c') %dopar% {
+    bait.cluster.tightness.matrix <- foreach(j = 2:10, .combine='cbind') %do%{
+        bait.cluster.tightness    <- foreach(i = 1:1000,.combine='c') %dopar% {
             bait.gene.expr.matrix          <- expr.matrix[,bait.gene]
             rd.gene                        <- sample(pool.gene,size=length(bait.gene) * alpha)
             bait.and.pool.gene.expr.matrix <- cbind(expr.matrix[,bait.gene],expr.matrix[,rd.gene])
             bait.and.pool.gene.cor.matrix  <- cor(bait.and.pool.gene.expr.matrix,method='spearman')
-            rs                             <- get.spectral.clustering.coordinates(bait.and.pool.gene.cor.matrix,k)
-            if(rs[['no.of.cluster']] == 1){
-                return(1/(1+alpha))
-            }
-        
+            rs                             <- get.spectral.clustering.coordinates(bait.and.pool.gene.cor.matrix,j)
+            
             coordinates                    <- rs[['coordinates']]
-            kmeans.obj                     <- kmeanspp(data=coordinates,k=ncol(coordinates))
+            kmeans.obj                     <- kmeanspp(data=coordinates,k=2)
             cluster.vec                    <- kmeans.obj$cluster
             names(cluster.vec)             <- rownames(coordinates)
             cluster.freq.df                <- table(cluster.vec[bait.gene]) %>% as.data.frame
-            cluster.freq.df                <- cluster.freq.df[order(cluster.freq.df$Freq,decreasing = T),]
+            cluster.freq.df                <- cluster.freq.df[order(cluster.freq.df$Freq,decreasing = TRUE),]
             value                          <- cluster.freq.df$Var1[1] %>% as.character %>% as.integer
             f1                             <- cluster.vec == value
             f2                             <- names(cluster.vec) %in% bait.gene
-            sum(f1 & f2)/sum(f1)
+            
+            d1 <- (dist(coordinates[f1,])   %>% as.matrix %>% sum)
+            d  <- (dist(coordinates)        %>% as.matrix %>% sum)
+            d1/d
         }
-        bait.cluster.purity
+        bait.cluster.tightness
     }
-    
+    colnames(bait.cluster.tightness.matrix) <- paste("no.of.eigen.vector=",2:10,sep="")
+    bait.cluster.tightness.matrix    
 }
 
 
@@ -93,14 +95,11 @@ do.gene.fishing <- function(bait.gene, expr.matrix,alpha=5,fishing.round=1000){
         rd.gene                        <- sub.pool
         bait.and.pool.gene.expr.matrix <- cbind(expr.matrix[,bait.gene],expr.matrix[,rd.gene])
         bait.and.pool.gene.cor.matrix  <- cor(bait.and.pool.gene.expr.matrix,method='spearman')
-        rs                             <- get.spectral.clustering.coordinates(bait.and.pool.gene.cor.matrix,method.name)
-        if(rs[['no.of.cluster']] == 1){
-            return('NOTHING')  
-        }
+        rs                             <- get.spectral.clustering.coordinates(bait.and.pool.gene.cor.matrix,2)
+        
         
         coordinates                    <- rs[['coordinates']]
-        print(ncol(coordinates))
-        kmeans.obj                     <- kmeanspp(data=coordinates,k=ncol(coordinates))
+        kmeans.obj                     <- kmeanspp(data=coordinates,k=2)
         cluster.vec                    <- kmeans.obj$cluster
         names(cluster.vec)             <- rownames(coordinates)
         cluster.freq.df                <- table(cluster.vec[bait.gene]) %>% as.data.frame
@@ -113,17 +112,22 @@ do.gene.fishing <- function(bait.gene, expr.matrix,alpha=5,fishing.round=1000){
         }
         names(cluster.vec)[f1 & !f2]
     }
+    
     fish.freq.df            <- table(fish.vec) %>% as.data.frame
     colnames(fish.freq.df)  <- c('fish.id','fish.freq')
     fish.freq.df$fish.freq  <- fish.freq.df$fish.freq / fishing.round
     fish.freq.df            <- arrange(fish.freq.df,desc(fish.freq))
     fish.freq.df            <- fish.freq.df[fish.freq.df$fish.id != 'NOTHING',]
+    fish.freq.df$fish.id    <- as.character(fish.freq.df$fish.id)
+    
     df                      <- data.frame(fish.id=setdiff(pool.gene,fish.freq.df$fish.id %>% as.character),fish.freq=0)
     fish.freq.df            <- rbind(fish.freq.df,df)
     p.hat                   <- mean(fish.freq.df$fish.freq)
     fish.freq.df$p.value    <- pbinom(fish.freq.df$fish.freq * fishing.round,fishing.round,p.hat,lower.tail = FALSE)
     fish.freq.df$adjusted.p.value <- p.adjust(fish.freq.df$p.value,method='bonferroni')
-    fish.freq.df 
+    colnames(fish.freq.df)        <- c('gene.id','CFR','p.value','adjusted.p.value')
+    f
+    ish.freq.df 
 }
 
 
