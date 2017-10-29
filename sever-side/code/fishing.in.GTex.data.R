2ec2fd8872c301030ddc5ad0cb39f7da7cca9c40
Ke Liu <keliu.iluke@gmail.com>
2017-10-28 20:15:32 -0700

    V1

--- sever-side/code/fishing.in.GTex.data.R
+++ sever-side/code/fishing.in.GTex.data.R
@@ -1,3 +1,4 @@
+load('raw.data/GTex/normalized.GTex.with.ncRNA.median.rpkm.larger.than.0.1.RData')
 source('code/gene.fishing.package.R')
 require(dplyr)
 require(foreach)
@@ -6,7 +7,23 @@ registerDoParallel(40)
 
 c1        <- read.table("./raw.data/c1.csv", header=FALSE,stringsAsFactors=FALSE)[,1]
 
-load('raw.data/GTex/normalized.GTex.with.ncRNA.median.rpkm.larger.than.0.1.RData')
+
+
+probe.result <- foreach( tissue  = names(normalized.GTex.data) )%do%{
+    tissue.expr.matrix <- normalized.GTex.data[[tissue]]
+    if(nrow(tissue.expr.matrix) <100){
+        fish <- c('NOTHING')
+        fish
+    }else{
+        probe.fishability(c1,tissue.expr.matrix)
+    }
+}
+names(probe.result) <- names(normalized.GTex.data)
+
+
+
+
+
 fish.list <- foreach( tissue  = names(normalized.GTex.data) )%do%{
     tissue.expr.matrix <- normalized.GTex.data[[tissue]]
     if(nrow(tissue.expr.matrix) <100){
@@ -18,40 +35,16 @@ fish.list <- foreach( tissue  = names(normalized.GTex.data) )%do%{
     }
 }
 names(fish.list) <- names(normalized.GTex.data)
-save(file='RData/fishing.in.GTex.with.ncRNA.median.rpkm.larger.than.0.1.RData',list=c('fish.list'))
 
 
 
-# load('raw.data/GTex/normalized.GTex.with.ncRNA.RData')
-# fish.list <- foreach( tissue  = names(normalized.GTex.data) )%do%{
-#   tissue.expr.matrix <- normalized.GTex.data[[tissue]]
-#   if(nrow(tissue.expr.matrix) <100){
-#     fish <- c('NOTHING')  
-#     fish
-#   }else{
-#     fish.freq.df <- do.gene.fishing.v3(bait.gene = c1,expr.matrix = tissue.expr.matrix,alpha=5,fishing.round=1000)
-#     fish.freq.df
-#   }
-# }
-# names(fish.list) <- names(normalized.GTex.data)
-# save(file='RData/fishing.in.GTex.with.ncRNA.at.least.10.sample.rpkm.larger.than.0.1.RData',list=c('fish.list'))
-# 
-# 
-# 
-# 
-# load('raw.data/GTex/normalized.GTex.RData')
-# fish.list <- foreach( tissue  = names(normalized.GTex.data) )%do%{
-#   tissue.expr.matrix <- normalized.GTex.data[[tissue]]
-#   if(nrow(tissue.expr.matrix) <100){
-#     fish <- c('NOTHING')  
-#     fish
-#   }else{
-#     fish.freq.df <- do.gene.fishing.v3(bait.gene = c1,expr.matrix = tissue.expr.matrix,alpha=5,fishing.round=1000)
-#     fish.freq.df
-#   }
-# }
-# names(fish.list) <- names(normalized.GTex.data)
-# save(file='RData/fishing.in.GTex.without.ncRNA.at.least.10.sample.rpkm.larger.than.0.1.RData',list=c('fish.list'))
+
+
+
+
+save(file='RData/fishing.in.GTex.with.ncRNA.median.rpkm.larger.than.0.1.RData',list=c('fish.list'，'probe.result'))
+
+
 
 
 
